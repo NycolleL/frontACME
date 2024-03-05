0f683b284ae0a24459885480332e29cb84fbf1a0
23114984 <lima.nyck01@gmail.com>
2024-03-05 12:21:13 -0300

    primeira pag

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-import { getFilmes, getFilme } from "./filmes.js";
+// import { getFilmes, getFilme } from "./filmes.js";
 
-function criarCard (filme) {
-    const card = document.createElement('div')
-    const titulo = document.createElement('h2')
-    titulo.textContent = filme.nome 
+// function criarCard (filme) {
+//     const card = document.createElement('div')
+//     const titulo = document.createElement('h2')
+//     titulo.textContent = filme.nome 
 
-    const valor = document.createElement("button")
-    valor.textContent = filme.valor_unitario
+//     const valor = document.createElement("button")
+//     valor.textContent = filme.valor_unitario
         
-        card.append(titulo, valor)
+//         card.append(titulo, valor)
 
-        return card
-    }
+//         return card
+//     }
 
-    async function preencherContainer () {
+//     async function preencherContainer () {
 
-        const container = document.querySelector('body')
-        const fimes = await getFilmes ()
+//         const container = document.querySelector('body')
+//         const fimes = await getFilmes ()
 
-        const filmes = [
-        {
-            nome: "Nycolle L."
-        },
+//         const filmes = [
+//         {
+//             nome: "Nycolle L."
+//         },
            
-        {
-            nome: "ok"
-        }
-    ]
+//         {
+//             nome: "ok"
+//         }
+//     ]
 
-        filmes.forEach (filme => {
-            const card = criarCard (filme)
-            container.appendChild (card)
-        })
-    }
+//         filmes.forEach (filme => {
+//             const card = criarCard (filme)
+//             container.appendChild (card)
+//         })
+//     }
 
-    preencherContainer()
+//     preencherContainer()
